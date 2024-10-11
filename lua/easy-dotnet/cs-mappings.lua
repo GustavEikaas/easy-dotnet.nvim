59492fb1ca40a9c503ce72a10a92cfadad167dfd
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-10-11 16:25:10 +0200

    feat: add sign and notify when run completed

--- lua/easy-dotnet/cs-mappings.lua
+++ lua/easy-dotnet/cs-mappings.lua
@@ -125,8 +125,42 @@ local function compare_paths(path1, path2)
   return normalized_path1 == normalized_path2
 end
 
+
+local function run_test(name, namespace, cs_project_path, cb)
+  local log_file_name = string.format("%s.xml", namespace:gsub("%b()", ""))
+  local normalized_path = vim.fs.normalize(cs_project_path)
+  local directory_path = vim.fs.dirname(normalized_path)
+  local relative_log_file_path = vim.fs.joinpath(directory_path, "TestResults", log_file_name)
+
+  local command = string.format(
+    "dotnet test --filter='%s' --nologo %s --logger='trx;logFileName=%s'",
+    namespace:gsub("%b()", ""), cs_project_path, log_file_name)
+
+  vim.fn.jobstart(
+    command, {
+      on_exit = function()
+        require("easy-dotnet.test-runner.test-parser").xml_to_json(relative_log_file_path,
+          ---@param unit_test_results TestCase
+          function(unit_test_results)
+            local result = unit_test_results[1]
+            if result == nil then
+              error(string.format("Status of %s was not present in xml file", name))
+            end
+            cb(result)
+          end)
+      end
+    })
+end
+
+
+
 M.add_test_signs = function()
   vim.fn.sign_define("EasyDotnetTestSign", { text = "", texthl = "Question" })
+  vim.fn.sign_define("EasyDotnetTestPassed", { text = "", texthl = "Question" })
+  vim.fn.sign_define("EasyDotnetTestFailed", { text = "", texthl = "Question" })
+  vim.fn.sign_define("EasyDotnetTestSkipped", { text = "", texthl = "Question" })
+  vim.fn.sign_define("EasyDotnetTestError", { text = "??", texthl = "Question" })
+
   vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = "*.cs",
     callback = function()
@@ -178,11 +212,43 @@ M.add_test_signs = function()
           end
           vim.notify("No tests found on this line")
         end, { silent = true, buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>r", function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          local curr_file = vim.api.nvim_buf_get_name(bufnr)
+          local current_line = vim.api.nvim_win_get_cursor(0)[1]
+          for _, value in ipairs(require("easy-dotnet.test-runner.runner").test_register) do
+            if compare_paths(value.file_path, curr_file) and value.line_number - 1 == current_line then
+              local spinner = require("easy-dotnet.ui-modules.spinner").new()
+              spinner:start_spinner("Running test")
+
+              run_test(value.name, value.namespace, value.cs_project_path, function(result)
+                if result.outcome == "Passed" then
+                  spinner:stop_spinner("Passed")
+                  vim.fn.sign_place(0, "EasyDotnetTestSignGroup", "EasyDotnetTestPassed", vim.api.nvim_get_current_buf(),
+                    { lnum = current_line - 1, priority = 20 })
+                elseif result.outcome == "Failed" then
+                  spinner:stop_spinner("Failed", vim.log.levels.ERROR)
+                  vim.fn.sign_place(0, "EasyDotnetTestSignGroup", "EasyDotnetTestFailed", vim.api.nvim_get_current_buf(),
+                    { lnum = current_line - 1, priority = 20 })
+                elseif result.outcome == "NotExecuted" then
+                  spinner:stop_spinner("Skipped", vim.log.levels.WARN)
+                  vim.fn.sign_place(0, "EasyDotnetTestSignGroup", "EasyDotnetTestSkipped", vim.api.nvim_get_current_buf(),
+                    { lnum = current_line - 1, priority = 20 })
+                else
+                  spinner:stop_spinner("Failed to validate test result", vim.log.levels.WARN)
+                  vim.fn.sign_place(0, "EasyDotnetTestSignGroup", "EasyDotnetTestError", vim.api.nvim_get_current_buf(),
+                    { lnum = current_line - 1, priority = 20 })
+                end
+              end)
+              return
+            end
+          end
+          vim.notify("No tests found on this line")
+        end, { silent = true, buffer = bufnr })
       end
     end
   })
 end
 
 return M
-eturn
-M
