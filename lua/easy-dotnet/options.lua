a0e228031efa1bc440e022d42a5173f7b499b80d
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-12-14 14:57:35 +0100

    remove print statement

--- lua/easy-dotnet/options.lua
+++ lua/easy-dotnet/options.lua
@@ -53,7 +53,6 @@ local function get_sdk_path()
 end
 
 local function get_secret_path(secret_guid)
-  print(secret_guid)
   local path = ""
   local home_dir = vim.fn.expand('~')
   if require("easy-dotnet.extensions").isWindows() then
