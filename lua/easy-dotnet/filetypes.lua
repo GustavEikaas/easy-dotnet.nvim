9a2e5775f6014d378de90e5001086bf0986d51d0
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2024-12-20 13:28:21 +0100

    Update lua/easy-dotnet/filetypes.lua

--- lua/easy-dotnet/filetypes.lua
+++ lua/easy-dotnet/filetypes.lua
@@ -2,18 +2,10 @@ local M = {}
 
 -- TODO: figure out how to properly create autocmd once and apply filetype to buffer once
 M.enable_filetypes = function()
-  if vim.b.did_ftplugin then
-    return
-  end
-
-  vim.b.did_ftplugin = true
-
-  vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = "*.props",
-    group = vim.api.nvim_create_augroup("solution_props", { clear = true }),
-    callback = function()
-      vim.bo.filetype = "xml"
-    end,
+  vim.filetype.add({
+    extension = {
+      props = "xml",
+    },
   })
 end
 
