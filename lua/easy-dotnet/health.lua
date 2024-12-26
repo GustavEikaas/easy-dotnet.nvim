1ca49e9a70a56f07c0631bb7eb1e0fb16d41e08e
GustavEikaas <gustav.eikaas@gmail.com>
2024-12-26 17:41:25 +0100

    .

--- lua/easy-dotnet/health.lua
+++ lua/easy-dotnet/health.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-
 ---@param command  string | table<string>
 ---@param advice  string | nil
 local function ensure_dep_installed(command, advice)
@@ -15,6 +14,17 @@ local function ensure_dep_installed(command, advice)
   end
 end
 
+
+local function ensure_nvim_dep_installed(pkg, advice)
+  local advice_lines = type(advice) == "string" and { advice } or advice
+  local success = pcall(function() require(pkg) end)
+  if not success then
+    vim.health.error(pkg .. " not installed", advice_lines)
+  else
+    vim.health.ok(pkg .. " installed")
+  end
+end
+
 local function measure_function(cb)
   local start_time = os.clock()
   cb()
@@ -24,12 +34,18 @@ local function measure_function(cb)
 end
 
 M.check = function()
-  vim.health.start("easy-dotnet dependencies")
-
+  vim.health.start("easy-dotnet CLI dependencies")
   ensure_dep_installed({ "dotnet", "-h" })
   ensure_dep_installed("jq")
   ensure_dep_installed({ "dotnet-outdated", "-h" }, "dotnet tool install --global dotnet-outdated-tool")
   ensure_dep_installed("dotnet-ef", "dotnet tool install --global dotnet-ef")
+
+  vim.health.start("easy-dotnet lua dependencies")
+  ensure_nvim_dep_installed("plenary", "https://github.com/nvim-lua/plenary.nvim")
+  ensure_nvim_dep_installed("dap", "https://github.com/mfussenegger/nvim-dap")
+  ensure_nvim_dep_installed("telescope", "https://github.com/nvim-telescope/telescope.nvim")
+  ensure_nvim_dep_installed("roslyn",
+    { "If you are using another LSP you can safely ignore this warning", "https://github.com/seblj/roslyn.nvim" })
 
   vim.health.start("easy-dotnet configuration")
   local config = require("easy-dotnet.options").options
@@ -37,9 +53,8 @@ M.check = function()
 
   if sdk_path_time > 1 then
     vim.health.warn(string.format("options.get_sdk_path took %d seconds", sdk_path_time),
-      "You should add get_sdk_path to your options for a performance improvement🚀")
+      "You should add get_sdk_path to your options for a performance improvement🚀. Check readme")
   end
 end
-
 
 return M
