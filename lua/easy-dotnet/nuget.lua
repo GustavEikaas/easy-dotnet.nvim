e18892734f990046db910cb8743eb19983a1c883
GustavEikaas <gustav.eikaas@gmail.com>
2024-12-26 15:15:08 +0100

    fix: coroutine yielding issues

--- lua/easy-dotnet/nuget.lua
+++ lua/easy-dotnet/nuget.lua
@@ -19,7 +19,9 @@ local function reverse_list(list)
 end
 
 ---@param cb function
-local function telescope_nuget_search(cb)
+local function telescope_nuget_search()
+  local co = coroutine.running()
+  local val
   local opts = {}
   pickers.new(opts, {
     prompt_title = "Nuget search",
@@ -47,11 +49,14 @@ local function telescope_nuget_search(cb)
       actions.select_default:replace(function(prompt_bufnr)
         local selection = action_state.get_selected_entry(prompt_bufnr)
         actions.close(prompt_bufnr)
-        cb(selection.value)
+        val = selection.value
+        coroutine.resume(co)
       end)
       return true
     end,
   }):find()
+  coroutine.yield()
+  return val
 end
 
 ---@param cb function
@@ -82,34 +87,34 @@ local function on_package_selected(package)
   local versions = polyfills.tbl_map(function(v)
     return { value = v, display = v }
   end, get_all_versions(package))
-  picker.picker(nil, versions, function(selected_version)
-    local sln_file_path = sln_parse.find_solution_file()
-    if not sln_file_path then
-      vim.notify("No solution file found")
-      return
+
+  local selected_version = picker.pick_sync(nil, versions, "Select a version", true)
+  local sln_file_path = sln_parse.find_solution_file()
+  if not sln_file_path then
+    vim.notify("No solution file found")
+    return
+  end
+  local projects = sln_parse.get_projects_from_sln(sln_file_path)
+  local selected_project = picker.pick_sync(nil, projects, "Select a project", true)
+  local command = string.format("dotnet add %s package %s --version %s", selected_project.path, package,
+    selected_version.value)
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code == 0 then
+        vim.notify(string.format("Installed %s@%s in %s", package, selected_version.value, selected_project.display))
+      else
+        vim.notify(
+          string.format("Failed to install %s@%s in %s", package, selected_version.value, selected_project.display),
+          vim.log.levels.ERROR)
+      end
     end
-    local projects = sln_parse.get_projects_from_sln(sln_file_path)
-    picker.picker(nil, projects, function(selected_project)
-      local command = string.format("dotnet add %s package %s --version %s", selected_project.path, package,
-        selected_version.value)
-      vim.fn.jobstart(command, {
-        on_exit = function(_, code)
-          if code == 0 then
-            vim.notify(string.format("Installed %s@%s in %s", package, selected_version.value, selected_project.display))
-          else
-            vim.notify(
-              string.format("Failed to install %s@%s in %s", package, selected_version.value, selected_project.display),
-              vim.log.levels.ERROR)
-          end
-        end
-      })
-    end, "Select a project", true)
-  end, "Select a version")
+  })
 end
 
 M.search_nuget = function()
   -- fzf_nuget_search(on_package_selected)
-  telescope_nuget_search(on_package_selected)
+  local package = telescope_nuget_search()
+  on_package_selected(package)
 end
 
 return M
