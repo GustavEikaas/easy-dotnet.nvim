7653b2e80d46ed5c5a493867aacc5833b0c7a6e2
Abatarus <giss1302@yandex.ru>
2025-01-28 07:57:56 +0300

    fix: correct picker check

--- lua/easy-dotnet/picker/init.lua
+++ lua/easy-dotnet/picker/init.lua
@@ -10,9 +10,9 @@ end
 
 ---@param cb function|nil
 M.search_nuget = function(cb)
-  if has_fzf then
+  if has_fzf() then
     return require("easy-dotnet.picker._fzf").nuget_search(cb)
-  elseif has_telescope then
+  elseif has_telescope() then
     return require("easy-dotnet.picker._telescope").nuget_search()
   else
     return require("easy-dotnet.picker._base").nuget_search()
@@ -20,18 +20,18 @@ M.search_nuget = function(cb)
 end
 
 M.migration_picker = function(opts, migration)
-  if has_fzf then
+  if has_fzf() then
     return require("easy-dotnet.picker._fzf").migration_picker(opts, migration)
-  elseif has_telescope then
+  elseif has_telescope() then
   else
     return require("easy-dotnet.picker._base").migration_picker(opts, migration)
   end
 end
 
 M.preview_picker = function(bufnr, options, on_select_cb, title, previewer, get_secret_path, readFile)
-  if has_fzf then
+  if has_fzf() then
     return require("easy-dotnet.picker._fzf").preview_picker(bufnr, options, on_select_cb, title, get_secret_path, readFile)
-  elseif has_telescope then
+  elseif has_telescope() then
     return require("easy-dotnet.picker._telescope").preview_picker(bufnr, options, on_select_cb, title, previewer)
   else
     return require("easy-dotnet.picker._base").preview_picker(bufnr, options, on_select_cb, title, previewer)
@@ -39,9 +39,9 @@ M.preview_picker = function(bufnr, options, on_select_cb, title, previewer, get_
 end
 
 M.picker = function(bufnr, options, on_select_cb, title, autopick)
-  if has_fzf then
+  if has_fzf() then
     return require("easy-dotnet.picker._fzf").picker(bufnr, options, on_select_cb, title, autopick)
-  elseif has_telescope then
+  elseif has_telescope() then
     return require("easy-dotnet.picker._telescope").picker(bufnr, options, on_select_cb, title, autopick)
   else
     return require("easy-dotnet.picker._base").picker(bufnr, options, on_select_cb, title, autopick)
@@ -49,9 +49,9 @@ M.picker = function(bufnr, options, on_select_cb, title, autopick)
 end
 
 M.pick_sync = function(bufnr, options, title, autopick)
-  if has_fzf then
+  if has_fzf() then
     return require("easy-dotnet.picker._fzf").pick_sync(bufnr, options, title, autopick)
-  elseif has_telescope then
+  elseif has_telescope() then
     return require("easy-dotnet.picker._telescope").pick_sync(bufnr, options, title, autopick)
   else
     return require("easy-dotnet.picker._base").pick_sync(bufnr, options, title, autopick)
