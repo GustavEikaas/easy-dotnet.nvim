2c0d5d07b4ef992833a45be44079a3af8db9d3b5
Gustav Eikaas <gustav.eikaas@gmail.com>
2025-05-01 10:37:23 +0200

    .

--- lua/easy-dotnet/actions/run.lua
+++ lua/easy-dotnet/actions/run.lua
@@ -7,24 +7,44 @@ local sln_parse = parsers.sln_parser
 local error_messages = require("easy-dotnet.error-messages")
 local polyfills = require("easy-dotnet.polyfills")
 
----@param use_default boolean
----@return DotnetProject, string | nil
+local pick_project_without_solution = function()
+  local csproject_path = csproj_parse.find_project_file()
+  if not csproject_path then logger.error(error_messages.no_runnable_projects_found) end
+  local project = csproj_parse.get_project_from_project_file(csproject_path)
+  local project_framework = picker.pick_sync(nil, project.get_all_runtime_definitions(), "Run project")
+  return project_framework
+end
+
+---@param term function
+local function csproj_fallback_run(term, args)
+  local project = pick_project_without_solution()
+  if project.type == "project_framework" then args = args .. " --framework " .. project.msbuild_props.targetFramework end
+  term(project.path, "run", args)
+end
+
+---Prompts the user to select a runnable DotnetProject (with framework),
+---optionally using a default if configured and allowed.
+---
+---This function looks for a solution file, checks if a default runnable project
+---is defined, and if so, uses it (if `use_default` is `true`). Otherwise, it
+---presents the user with a picker of all runnable projects and their frameworks.
+---If no solution file is found, falls back to picking a project without one.
+---
+---If a project is selected, the default is updated for future invocations.
+---
+---@param use_default boolean: If true, allows using the stored default project if available.
+---@return DotnetProject: The selected or default DotnetProject.
+---@return string|nil: The path to the solution file, or nil if no solution is used.
 local function pick_project_framework(use_default)
   local default_manager = require("easy-dotnet.default-manager")
   local solution_file_path = sln_parse.find_solution_file()
-  if solution_file_path == nil then
-    local csproject_path = csproj_parse.find_project_file()
-    if not csproject_path then logger.error(error_messages.no_runnable_projects_found) end
-    local project = csproj_parse.get_project_from_project_file(csproject_path)
-    local project_framework = picker.pick_sync(nil, project.get_all_runtime_definitions(), "Run project")
-    return project_framework, nil
-  end
+  if solution_file_path == nil then return pick_project_without_solution(), nil end
 
   local default = default_manager.check_default_project(solution_file_path, "run")
 
   if default ~= nil and use_default == true then
     if default.type == "solution" then error("Type solution is not supported for dotnet run") end
-    return default.project
+    return default.project, solution_file_path
   end
 
   local projects = sln_parse.get_projects_and_frameworks_flattened_from_sln(solution_file_path, function(i) return i.runnable == true end)
@@ -40,20 +60,6 @@ local function pick_project_framework(use_default)
   return project_framework, solution_file_path
 end
 
----@param term function
-local function csproj_fallback(term, args)
-  local csproj_path = csproj_parse.find_project_file()
-  if csproj_path == nil then
-    logger.error(error_messages.no_project_definition_found)
-    return
-  end
-  local project = csproj_parse.get_project_from_project_file(csproj_path)
-  picker.picker(nil, project.get_all_runtime_definitions(), function(i)
-    if i.type == "project_framework" then args = args .. " --framework " .. i.msbuild_props.targetFramework end
-    term(i.path, "run", args)
-  end, "Run project")
-end
-
 ---@param term function | nil
 ---@param use_default boolean | nil
 ---@param args string | nil
@@ -64,29 +70,15 @@ M.run_project_picker = function(term, use_default, args)
   local default_manager = require("easy-dotnet.default-manager")
   local solution_file_path = sln_parse.find_solution_file()
   if solution_file_path == nil then
-    csproj_fallback(term, args)
+    csproj_fallback_run(term, args)
     return
   end
 
-  local default = default_manager.check_default_project(solution_file_path, "run")
-  if default ~= nil and use_default == true then
-    if default.type == "solution" then error("Type solution is not supported for dotnet run") end
-    if default.project.msbuild_props.isMultiTarget then args = args .. " --framework " .. default.project.msbuild_props.targetFramework end
-    term(default.path, "run", args)
-    return
-  end
+  local project = pick_project_framework(use_default)
+  if project.msbuild_props.isMultiTarget then args = args .. " --framework " .. project.msbuild_props.targetFramework end
+  term(project.path, "run", args)
 
-  local project_frameworks = sln_parse.get_projects_and_frameworks_flattened_from_sln(solution_file_path, function(i) return i.runnable == true end)
-
-  if #project_frameworks == 0 then
-    logger.error(error_messages.no_runnable_projects_found)
-    return
-  end
-  picker.picker(nil, project_frameworks, function(i)
-    if i.type == "project_framework" then args = args .. " --framework " .. i.msbuild_props.targetFramework end
-    term(i.path, "run", args)
-    default_manager.set_default_project(i, solution_file_path, "run")
-  end, "Run project")
+  if not use_default then default_manager.set_default_project(project, solution_file_path, "run") end
 end
 
 ---@param project DotnetProject
