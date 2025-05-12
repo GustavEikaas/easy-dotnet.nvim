dbc7013e25939abc4b7e143e041248ebaaa8adae
GustavEikaas <gustav.eikaas@gmail.com>
2025-05-12 22:49:01 +0200

    fix: quote paths in terminal callbacks

--- lua/easy-dotnet/options.lua
+++ lua/easy-dotnet/options.lua
@@ -90,11 +90,11 @@ local M = {
     terminal = function(path, action, args)
       args = args or ""
       local commands = {
-        run = function() return string.format("dotnet run --project %s %s", path, args) end,
-        test = function() return string.format("dotnet test %s %s", path, args) end,
-        restore = function() return string.format("dotnet restore %s %s", path, args) end,
-        build = function() return string.format("dotnet build %s %s", path, args) end,
-        watch = function() return string.format("dotnet watch --project %s %s", path, args) end,
+        run = function() return string.format("dotnet run --project '%s' %s", path, args) end,
+        test = function() return string.format("dotnet test '%s' %s", path, args) end,
+        restore = function() return string.format("dotnet restore '%s' %s", path, args) end,
+        build = function() return string.format("dotnet build '%s' %s", path, args) end,
+        watch = function() return string.format("dotnet watch --project '%s' %s", path, args) end,
       }
       local command = commands[action]()
       if require("easy-dotnet.extensions").isWindows() == true then command = command .. "\r" end
