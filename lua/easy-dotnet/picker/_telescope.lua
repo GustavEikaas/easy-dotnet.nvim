52365c330ee51190fcc1d0b218d0408ea82ef1cb
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2025-05-13 09:12:16 +0200

    Update lua/easy-dotnet/picker/_telescope.lua

--- lua/easy-dotnet/picker/_telescope.lua
+++ lua/easy-dotnet/picker/_telescope.lua
@@ -138,9 +138,6 @@ M.picker = function(bufnr, options, on_select_cb, title, autopick, apply_numerat
     })
   end
 
-  require("snacks").debug(options)
-  -- require("snacks").debug(options_for_finder)
-
   local picker = require("telescope.pickers").new(bufnr, {
     prompt_title = title,
     finder = require("telescope.finders").new_table({
