032297ad84b6b522158bf2a891b0a85bcd6331a5
bosvik <cbosvik@icloud.com>
2025-05-21 08:02:30 +0200

    squash! fix: resolve pipe path on macOS
    # When applied, this commit will (in less than 72 characters)...
    
    # Why is this change needed?
    
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other relevant resources

--- lua/easy-dotnet/test-runner/runner.lua
+++ lua/easy-dotnet/test-runner/runner.lua
@@ -109,9 +109,14 @@ local function start_server(solution_file_path)
             local pipename = line:sub(#server_ready_prefix + 1)
             M._server.pipe_name = vim.trim(pipename)
             M._server.client = require("easy-dotnet.test-runner.rpc-client")
-            local full_pipe_path = extensions.isWindows() and [[\\.\pipe\]] .. M._server.pipe_name
-              or "/tmp/CoreFxPipe_" .. M._server.pipe_name
-              or os.getenv("TMPDIR") .. "CoreFxPipe" .. M._server.pipe_name
+            local full_pipe_path
+            if extensions.isWindows() then
+              full_pipe_path = [[\\.\pipe\]] .. M._server.pipe_name
+            elseif extensions.isDarwin() then
+              full_pipe_path = os.getenv("TMPDIR") .. "CoreFxPipe" .. M._server.pipe_name
+            else
+              full_pipe_path = "/tmp/CoreFxPipe_" .. M._server.pipe_name
+            end
 
             is_negotiating = true
             M._server.client.setup({ pipe_path = full_pipe_path, debug = false })
