320d945d7f31e0ea63dbc4aa1c1125a9df592c06
GustavEikaas <gustav.eikaas@gmail.com>
2025-08-06 19:02:59 +0200

    refactor(netcoredbg): add interface for value converters

---
+++ lua/easy-dotnet/netcoredbg/value_converters/init.lua
@@ -0,0 +1,69 @@
+---@class ValueConverter
+---@field extract fun(stack_frame_id: integer, vars: Variable[], var_path: string, var_type: string, cb: fun(result: table, pretty_string: string, highlight?: string))
+---@field satisfies_type fun(var_type: string, vars: Variable[]): boolean
+
+local M = {}
+
+---@type ValueConverter[]
+M.value_converters = {
+  require("easy-dotnet.netcoredbg.value_converters.exception"),
+  require("easy-dotnet.netcoredbg.value_converters.jobject"),
+  require("easy-dotnet.netcoredbg.value_converters.jarray"),
+  require("easy-dotnet.netcoredbg.value_converters.jvalue"),
+  require("easy-dotnet.netcoredbg.value_converters.jproperty"),
+  require("easy-dotnet.netcoredbg.value_converters.guid"),
+  require("easy-dotnet.netcoredbg.value_converters.list"),
+  require("easy-dotnet.netcoredbg.value_converters.sorted_list"),
+  require("easy-dotnet.netcoredbg.value_converters.immutable_list"),
+  require("easy-dotnet.netcoredbg.value_converters.readonly_list"),
+  require("easy-dotnet.netcoredbg.value_converters.tuple"),
+  require("easy-dotnet.netcoredbg.value_converters.hashset"),
+  require("easy-dotnet.netcoredbg.value_converters.queue"),
+  require("easy-dotnet.netcoredbg.value_converters.stack"),
+  require("easy-dotnet.netcoredbg.value_converters.dictionaries"),
+  require("easy-dotnet.netcoredbg.value_converters.readonly_dictionary"),
+  require("easy-dotnet.netcoredbg.value_converters.concurrent_dictionary"),
+}
+
+---Converts a list of DAP variables into a Lua table.
+---Numeric-looking keys like [0], [1] go into array part.
+---Named keys go into map part.
+---
+---@param vars table[] # List of DAP variable tables with .name and .value
+local function vars_to_table(var_path, vars, cb)
+  local result = {}
+
+  for _, c in ipairs(vars) do
+    local index = c.name:match("^%[(%d+)%]$")
+    if index then
+      c.var_path = var_path .. c.name
+      table.insert(result, c)
+    else
+      c.var_path = var_path .. "." .. c.name
+      result[c.name] = c
+    end
+  end
+  cb(result, require("easy-dotnet.netcoredbg.pretty_printers.catch-all").pretty_print(result))
+end
+
+---Resolves any C# type and invokes the given callback with the lua result
+---@param stack_frame_id integer
+---@param vars Variable[]
+---@param var_path string
+---@param var_type string
+---@param cb fun(result: table, pretty_string: string, highlight?: string)
+M.extract = function(stack_frame_id, vars, var_path, var_type, cb)
+  ---@param r ValueConverter
+  ---@type ValueConverter[]
+  local matches = vim.iter(M.value_converters):filter(function(r) return r.satisfies_type(var_type, vars) end):totable()
+
+  if #matches > 1 then
+    error("More than one value converter found for type " .. var_type)
+  elseif #matches == 1 then
+    matches[1].extract(stack_frame_id, vars, var_path, var_type, cb)
+  else
+    vars_to_table(var_path, vars, cb)
+  end
+end
+
+return M
--- lua/easy-dotnet/netcoredbg/value_converters/readonly_dictionary.lua
+++ lua/easy-dotnet/netcoredbg/value_converters/readonly_dictionary.lua
@@ -1,17 +1,15 @@
-local M = {}
-
-M.is_readonly_dictionary = function(class_name) return type(class_name) == "string" and class_name:match("^System%.Collections%.ObjectModel%.ReadOnlyDictionary") end
-
-function M.extract(var_path, vars, cb)
-  for _, entry in ipairs(vars) do
-    if entry.name == "Dictionary" and entry.variablesReference ~= 0 then
-      require("easy-dotnet.netcoredbg").fetch_variables(
-        entry.variablesReference,
-        1,
-        function(children) return require("easy-dotnet.netcoredbg.value_converters.dictionaries").extract(var_path .. ".Dictionary", children, cb) end
-      )
+---@type ValueConverter
+return {
+  satisfies_type = function(class_name) return type(class_name) == "string" and class_name:match("^System%.Collections%.ObjectModel%.ReadOnlyDictionary") end,
+  extract = function(frame_id, vars, var_path, _, cb)
+    for _, entry in ipairs(vars) do
+      if entry.name == "Dictionary" and entry.variablesReference ~= 0 then
+        require("easy-dotnet.netcoredbg").fetch_variables(
+          entry.variablesReference,
+          1,
+          function(children) return require("easy-dotnet.netcoredbg.value_converters.dictionaries").extract(frame_id, children, var_path .. ".Dictionary", entry.type, cb) end
+        )
+      end
     end
-  end
-end
-
-return M
+  end,
+}
