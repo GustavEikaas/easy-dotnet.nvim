ee07b35e6d12e305757a87b1625bda74878f382b
GustavEikaas <gustav.eikaas@gmail.com>
2025-08-06 21:29:55 +0200

    .

--- lua/easy-dotnet/netcoredbg/value_converters/uri.lua
+++ lua/easy-dotnet/netcoredbg/value_converters/uri.lua
@@ -5,9 +5,5 @@ return {
     if type(class_name) ~= "string" then return false end
     return class_name:match("^System%.Uri$") ~= nil
   end,
-  extract = function(_, vars, _, _, cb)
-    for _, value in ipairs(vars) do
-      if value.name == "_string" then cb({ value = value }, value.value) end
-    end
-  end,
+  extract = function(frame_id, vars, var_path, _, cb) require("easy-dotnet.netcoredbg.value_converters").simple_unwrap("_string", frame_id, vars, var_path, cb) end,
 }
