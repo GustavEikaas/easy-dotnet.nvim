34a9f14d31eb475bf6b36b569f5e4ebf6b9febff
GustavEikaas <gustav.eikaas@gmail.com>
2025-09-10 20:05:34 +0200

    .

--- lua/easy-dotnet/rpc/server.lua
+++ lua/easy-dotnet/rpc/server.lua
@@ -62,9 +62,7 @@ function M.start(cb)
           local pipe_name = vim.trim(pipename)
           local full_pipe_path
           if extensions.isWindows() then
-            -- full_pipe_path = [[\\.\pipe\]] .. pipe_name
-
-            full_pipe_path = [[\\.\pipe\EasyDotnet_ROcrjwn9kiox3tKvRWcQg]]
+            full_pipe_path = [[\\.\pipe\]] .. pipe_name
           elseif extensions.isDarwin() then
             full_pipe_path = os.getenv("TMPDIR") .. "CoreFxPipe_" .. pipe_name
           else
