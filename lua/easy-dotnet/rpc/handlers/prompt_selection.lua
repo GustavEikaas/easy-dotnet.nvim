3126f4bbd1be8ba8560a1348f2f7ab6e02db5d70
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-17 18:13:57 +0200

    .

--- lua/easy-dotnet/rpc/handlers/prompt_selection.lua
+++ lua/easy-dotnet/rpc/handlers/prompt_selection.lua
@@ -4,7 +4,19 @@ return function(params, response, throw, validate)
     throw({ code = -32602, message = err })
     return
   end
-  ---@type RPC_PromptSelection[]
-  local options = params.choices
+  local options = vim.deepcopy(params.choices)
+  local defaultId = params.defaultSelectionId
+
+  if defaultId then vim.iter(options):map(function(option)
+    if option.id == defaultId then option.display = option.display .. " (default)" end
+    return option
+  end) end
+
+  if defaultId then table.sort(options, function(a, b)
+    if a.id == defaultId then return true end
+    if b.id == defaultId then return false end
+    return false
+  end) end
+
   require("easy-dotnet.picker").picker(nil, options, function(i) response(i.id) end, params.prompt, true, true)
 end
