b09f5b5cf13776fdc0a0ea74931dd4fe9fb194a9
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-19 08:52:59 +0200

    .

--- lua/easy-dotnet/picker/_snacks.lua
+++ lua/easy-dotnet/picker/_snacks.lua
@@ -146,8 +146,8 @@ M.picker = function(options, on_select_cb, title, autopick, apply_numeration)
   })
 end
 
----@diagnostic disable-next-line: unused-local
-M.multi_picker = function(options, on_select_cb, title, apply_numeration) error("multi_picker not implemented for snacks") end
+-- (options, on_select_cb, title, apply_numeration)
+M.multi_picker = function() error("multi_picker not implemented for snacks") end
 
 ---@generic T
 ---@param options table<T>
