e2a737674cb684069e76a59e0187a37710144f00
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-11-13 19:14:29 +0100

    feat(ef): add error handling (#627)

--- lua/easy-dotnet/ef-core/database.lua
+++ lua/easy-dotnet/ef-core/database.lua
@@ -1,3 +1,4 @@
+local logger = require("easy-dotnet.logger")
 local job = require("easy-dotnet.ui-modules.jobs")
 local M = {}
 
@@ -10,6 +11,11 @@ M.database_update = function(mode)
   if mode == "pick" then
     local cmd = string.format("dotnet ef migrations list --prefix-output --project %s --startup-project %s", project.path, startup_project.path)
     local stdout = vim.fn.system(cmd)
+    local exit_code = vim.v.shell_error
+    if exit_code ~= 0 then
+      logger.error(stdout)
+      return
+    end
     local migrations = {}
     local lines = {}
     for line in stdout:gmatch("[^\r\n]+") do
@@ -31,9 +37,15 @@ M.database_update = function(mode)
 
   local migrations_job = job.register_job({ name = "Applying migrations", on_error_text = "Database update failed", on_success_text = "Database update failed" })
   local cmd = string.format("dotnet ef database update %s --project %s --startup-project %s", selected_migration.value, project.path, startup_project.path)
+  local stdout = {}
 
   vim.fn.jobstart(cmd, {
-    on_exit = function(_, code) migrations_job(code == 0) end,
+    on_stdout = function(_, data) vim.list_extend(stdout, data) end,
+    on_stderr = function(_, data) vim.list_extend(stdout, data) end,
+    on_exit = function(_, code)
+      if code ~= 0 then logger.error(table.concat(stdout, "\n")) end
+      migrations_job(code == 0)
+    end,
   })
 end
 --
@@ -44,8 +56,19 @@ M.database_drop = function()
 
   local drop_job = job.register_job({ name = "Dropping database", on_error_text = "Failed to drop database", on_success_text = "Database dropped" })
   local cmd = string.format("dotnet ef database drop --project %s --startup-project %s -f", project.path, startup_project.path)
+  local stdout = {}
+
   vim.fn.jobstart(cmd, {
-    on_exit = function(_, code) drop_job(code == 0) end,
+    on_stdout = function(_, data)
+      if data then vim.list_extend(stdout, data) end
+    end,
+    on_stderr = function(_, data)
+      if data then vim.list_extend(stdout, data) end
+    end,
+    on_exit = function(_, code)
+      if code ~= 0 then logger.error(table.concat(stdout, "\n")) end
+      drop_job(code == 0)
+    end,
   })
 end
 
