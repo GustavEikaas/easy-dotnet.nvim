541a823bab5c457379db431352a9404b6c9025ed
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-12-27 23:18:35 +0100

    feat(rpc): set quickfix list (#700)

--- lua/easy-dotnet/rpc/notification-handler.lua
+++ lua/easy-dotnet/rpc/notification-handler.lua
@@ -1,6 +1,7 @@
 local jobs = require("easy-dotnet.ui-modules.jobs")
 local logger = require("easy-dotnet.logger")
 local csproj_parse = require("easy-dotnet.parsers.csproj-parse")
+local constants = require("easy-dotnet.constants")
 local M = {}
 
 local active_server_finish_callbacks = {}
@@ -50,6 +51,32 @@ M.handler = function(client, method, params)
       logger.warn(params.message)
     elseif method == "displayMessage" then
       logger.info(params.message)
+    elseif method == "quickfix/set" then
+      local items = vim.tbl_map(
+        function(value)
+          return {
+            filename = value.fileName,
+            lnum = value.lineNumber,
+            col = value.columnNumber,
+            text = value.text,
+            type = value.type == 2 and "E" or value.type == 1 and "W" or "I",
+          }
+        end,
+        params
+      )
+
+      vim.fn.setqflist({}, " ", {
+        title = constants.server_quickfix_title,
+        items = items,
+      })
+      vim.cmd("copen")
+    elseif method == "quickfix/close" then
+      local info = vim.fn.getqflist({ title = 1 })
+      local our_qf = info.title == constants.server_quickfix_title
+      if our_qf then
+        vim.fn.setqflist({})
+        vim.cmd("cclose")
+      end
     else
       vim.print("Unknown server notification " .. method)
     end
