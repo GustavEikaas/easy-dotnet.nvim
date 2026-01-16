8989c872ecf7c757ffa6d50b0e314d1f051598fb
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-01-16 15:13:12 +0100

    feat: improve healthcheck (#738)

--- lua/easy-dotnet/health.lua
+++ lua/easy-dotnet/health.lua
@@ -1,3 +1,4 @@
+local constants = require("easy-dotnet.constants")
 local options = require("easy-dotnet.options")
 --checkhealth easy-dotnet
 local M = {}
@@ -35,25 +36,23 @@ local function ensure_nvim_dep_installed(pkg, advice, required)
 end
 
 local function check_debugger_configured()
+  local auto_register = options.get_option("debugger").auto_register_dap
+  if auto_register == false then
+    vim.health.warn("auto_register_dap config is disabled, you're on your own")
+    return
+  end
   local success, s = pcall(function() return require("dap") end)
   if not success or not s then
     --verifying nvim-dap is done in another check
     return
   end
   for key, _ in pairs(s.adapters) do
-    if key == "easy-dotnet" then
+    if key == constants.debug_adapter_name then
       vim.health.ok("debugger is configured")
       return
     end
   end
-  if options.get_option("debugger").bin_path == nil then
-    vim.health.error(
-      "debugger is not configured because `options.debugger.bin_path` was not supplied",
-      { "https://github.com/GustavEikaas/easy-dotnet.nvim?tab=readme-ov-file#nvim-dap-configuration" }
-    )
-  else
-    vim.health.error("debugger not configured, is your debug config overwriting dap config?", { "https://github.com/GustavEikaas/easy-dotnet.nvim?tab=readme-ov-file#nvim-dap-configuration" })
-  end
+  vim.health.error("Unable to find auto_registered_dap_config, is it being overwritten?", { "https://github.com/GustavEikaas/easy-dotnet.nvim?tab=readme-ov-file#nvim-dap-configuration" })
 end
 
 local function check_cmp()
