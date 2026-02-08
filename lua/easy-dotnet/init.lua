8068eec96502de087f830f882bd357405173db46
GustavEikaas <gustav.eikaas@gmail.com>
2026-02-08 12:25:28 +0100

    .

--- lua/easy-dotnet/init.lua
+++ lua/easy-dotnet/init.lua
@@ -148,12 +148,12 @@ local function auto_register_dap(merged_opts)
     end
     require("easy-dotnet.netcoredbg.sys_monitor_dap_ui").setup()
 
-    dap.listeners.after["event_telemetry/cpu"] = {
-      [constants.debug_adapter_name] = function(session, body) require("easy-dotnet.netcoredbg.sys_monitor").route_message("telemetry/cpu", { value = body.Value, timestamp = body.Timestamp }) end,
-    }
-
-    dap.listeners.after["event_telemetry/mem"] = {
-      [constants.debug_adapter_name] = function(session, body) require("easy-dotnet.netcoredbg.sys_monitor").route_message("telemetry/mem", { value = body.Value, timestamp = body.Timestamp }) end,
+    dap.listeners.after["event_telemetry/metrics"] = {
+      [constants.debug_adapter_name] = function(_, body)
+        if not body then return end
+        require("easy-dotnet.netcoredbg.sys_monitor").route_message("telemetry/cpu", { value = body.cpuPercent, timestamp = body.timestamp })
+        require("easy-dotnet.netcoredbg.sys_monitor").route_message("telemetry/mem", { value = body.memoryBytes, timestamp = body.timestamp })
+      end,
     }
   end
 end
