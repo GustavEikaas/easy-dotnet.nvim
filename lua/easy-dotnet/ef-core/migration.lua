5cd408e817c25de371735263f88e9553b90424aa
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-02-14 07:36:10 +0100

    chore: remove unused ef functions (#790)

--- lua/easy-dotnet/ef-core/migration.lua
+++ lua/easy-dotnet/ef-core/migration.lua
@@ -2,56 +2,6 @@ local logger = require("easy-dotnet.logger")
 local job = require("easy-dotnet.ui-modules.jobs")
 local M = {}
 
----@param migration_name string
-M.add_migration = function(migration_name)
-  if not migration_name then
-    logger.error("Migration name required")
-    return
-  end
-  local selections = require("easy-dotnet.ef-core.utils").pick_projects()
-  local project = selections.project
-  local startup_project = selections.startup_project
-
-  local cmd = string.format("dotnet ef migrations add %s --project %s --startup-project %s", migration_name, project.path, startup_project.path)
-
-  local stdout = {}
-  local migration_job = job.register_job({ name = "Adding migration", on_error_text = "Failed to add migration", on_success_text = "Migration added" })
-  vim.fn.jobstart(cmd, {
-    on_stdout = function(_, data)
-      if data then vim.list_extend(stdout, data) end
-    end,
-    on_stderr = function(_, data)
-      if data then vim.list_extend(stdout, data) end
-    end,
-    on_exit = function(_, code)
-      if code ~= 0 then logger.error(table.concat(stdout, "\n")) end
-      migration_job(code == 0)
-    end,
-  })
-end
-
-M.remove_migration = function()
-  local selections = require("easy-dotnet.ef-core.utils").pick_projects()
-  local project = selections.project
-  local startup_project = selections.startup_project
-
-  local cmd = string.format("dotnet ef migrations remove --project %s --startup-project %s", project.path, startup_project.path)
-  local stdout = {}
-  local migration_job = job.register_job({ name = "Removing migration", on_error_text = "Failed to remove migration", on_success_text = "Migration removed" })
-  vim.fn.jobstart(cmd, {
-    on_stdout = function(_, data)
-      if data then vim.list_extend(stdout, data) end
-    end,
-    on_stderr = function(_, data)
-      if data then vim.list_extend(stdout, data) end
-    end,
-    on_exit = function(_, code)
-      if code ~= 0 then logger.error(table.concat(stdout, "\n")) end
-      migration_job(code == 0)
-    end,
-  })
-end
-
 M.list_migrations = function()
   local selections = require("easy-dotnet.ef-core.utils").pick_projects()
   local project = selections.project
