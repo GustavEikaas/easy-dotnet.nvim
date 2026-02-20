ef1f4ad2ebaa6d9bd2811d2d77af73c6bac19a71
Gustav Eikaas <gustav.eikaas@gmail.com>
2026-02-20 10:11:16 +0100

    feat(nvim): add json5 filetype detection for .NET settings

--- lua/easy-dotnet/filetypes.lua
+++ lua/easy-dotnet/filetypes.lua
@@ -2,6 +2,14 @@ local M = {}
 
 M.enable_filetypes = function()
   vim.filetype.add({
+    filename = {
+      ["secrets.json"] = "json5",
+      ["launchSettings.json"] = "json5",
+      ["appsettings.json"] = "json5",
+    },
+    pattern = {
+      ["appsettings%.%a+%.json"] = "json5",
+    },
     extension = {
       props = "xml",
     },
