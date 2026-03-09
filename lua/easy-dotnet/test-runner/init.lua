b31ac3cd9cb45ab9c10201509c0dfd169237b656
Gustav Eikaas <gustav.eikaas@gmail.com>
2026-03-09 11:09:46 +0100

    avoid initialize on every testrun open

--- lua/easy-dotnet/test-runner/init.lua
+++ lua/easy-dotnet/test-runner/init.lua
@@ -4,7 +4,11 @@ local function do_initialize(client, solution_path)
   local state = require("easy-dotnet.test-runner.state")
   client:initialize(function()
     state.active_handle = client.testrunner:initialize(solution_path, function(result)
-      if not result or not result.success then require("easy-dotnet.logger").error("Test runner initialization failed") end
+      if result and result.success then
+        state.initialized = true
+      else
+        require("easy-dotnet.logger").error("Test runner initialization failed")
+      end
     end)
   end)
 end
@@ -24,6 +28,7 @@ end
 function M.open()
   local options = require("easy-dotnet.options").get_option("test_runner")
   local render = require("easy-dotnet.test-runner.render")
+  local state = require("easy-dotnet.test-runner.state")
   local current_solution = require("easy-dotnet.current_solution")
   local client = require("easy-dotnet.rpc.rpc").global_rpc_client
 
@@ -35,8 +40,11 @@ function M.open()
   render.open(options.viewmode, options)
   require("easy-dotnet.test-runner.keymaps").register(render.buf, client, options)
 
+  if state.initialized then return end
+
   current_solution.get_or_pick_solution(function(solution_path)
-    if solution_path then do_initialize(client, solution_path) end
+    if not solution_path then return end
+    do_initialize(client, solution_path)
   end)
 end
 
