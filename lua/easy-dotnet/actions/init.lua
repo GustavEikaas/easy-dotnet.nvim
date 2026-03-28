fb071a547d6a6ba8d8e5e3afdd5a93becce7d0bf
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-03-28 20:14:02 +0100

    feat: workspace test (#865)

--- lua/easy-dotnet/actions/init.lua
+++ lua/easy-dotnet/actions/init.lua
@@ -1,8 +1,5 @@
 local M = {}
 
-M.test = require("easy-dotnet.actions.test").run_test_picker
-M.test_solution = require("easy-dotnet.actions.test").test_solution
-M.test_watcher = require("easy-dotnet.actions.test").test_watcher
 M.watch = require("easy-dotnet.actions.watch").run_project_picker
 M.pack = require("easy-dotnet.actions.pack").pack
 M.pack_and_push = require("easy-dotnet.actions.pack").push
