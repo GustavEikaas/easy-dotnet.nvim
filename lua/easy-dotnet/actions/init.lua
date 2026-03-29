333c50b69e8e70dbe112adc2a2e3146f054edf66
GustavEikaas <gustav.eikaas@gmail.com>
2026-03-29 08:58:47 +0200

    chore: remove unused

--- lua/easy-dotnet/actions/init.lua
+++ lua/easy-dotnet/actions/init.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-M.watch = require("easy-dotnet.actions.watch").run_project_picker
 M.pack = require("easy-dotnet.actions.pack").pack
 M.pack_and_push = require("easy-dotnet.actions.pack").push
 
