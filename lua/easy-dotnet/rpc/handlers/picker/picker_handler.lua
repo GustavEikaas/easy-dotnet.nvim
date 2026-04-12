7e48d2ed7e76184955f8b5858512d789ba50f7ea
GustavEikaas <gustav.eikaas@gmail.com>
2026-04-12 19:47:13 +0200

    .

--- lua/easy-dotnet/rpc/handlers/picker/picker_handler.lua
+++ lua/easy-dotnet/rpc/handlers/picker/picker_handler.lua
@@ -1,11 +1,7 @@
 local M = {}
 
-M.pick = function(params, response, _throw, _validate)
-  require("easy-dotnet.picker").server_picker(params, response)
-end
+M.pick = function(params, response, _throw, _validate) require("easy-dotnet.picker").server_picker(params, response) end
 
-M.live = function(params, response, _throw, _validate)
-  require("easy-dotnet.picker").server_live(params, response)
-end
+M.live = function(params, response, _throw, _validate) require("easy-dotnet.picker").server_live(params, response) end
 
 return M
