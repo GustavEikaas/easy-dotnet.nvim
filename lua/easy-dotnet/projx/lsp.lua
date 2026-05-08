9466ecb74b7123b6d2f6c7dff7776e2e36f8b635
GustavEikaas <gustav.eikaas@gmail.com>
2026-05-08 15:58:44 +0200

    fix(projx) lsp crashing on virtual files

--- lua/easy-dotnet/projx/lsp.lua
+++ lua/easy-dotnet/projx/lsp.lua
@@ -5,6 +5,15 @@ local M = {
   state = {},
 }
 
+vim.lsp.commands["easy-dotnet.openFile"] = function(command)
+  local path = command.arguments and command.arguments[1]
+  if type(path) ~= "string" or path == "" then
+    vim.notify("[easy-dotnet] openFile: missing path", vim.log.levels.WARN)
+    return
+  end
+  vim.cmd("edit " .. vim.fn.fnameescape(path))
+end
+
 function M.enable()
   if vim.fn.has("nvim-0.11") == 0 then
     logger.warn("easy-dotnet LSP requires neovim 0.11 or higher ")
@@ -15,7 +24,9 @@ function M.enable()
     cmd = { "dotnet-easydotnet", "projx-language-server" },
     filetypes = { "xml" },
     root_dir = function(buf_nr, cb)
-      local buf_path = vim.fs.normalize(vim.api.nvim_buf_get_name(buf_nr))
+      local buf_path = vim.api.nvim_buf_get_name(buf_nr)
+      if buf_path:match("^%a+://") then return cb(nil) end
+      if vim.fn.filereadable(buf_path) == 0 then return cb(nil) end
       local root_dir = vim.fs.dirname(buf_path)
       cb(root_dir)
     end,
