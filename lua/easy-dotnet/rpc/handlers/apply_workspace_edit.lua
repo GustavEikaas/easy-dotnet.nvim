5e8b68c909e21550af19f682dccf6c5b307f63be
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-08-14 14:09:57 +0200

    make it backwards compatible

--- lua/easy-dotnet/rpc/handlers/apply_workspace_edit.lua
+++ lua/easy-dotnet/rpc/handlers/apply_workspace_edit.lua
@@ -7,9 +7,12 @@ return function(params, response, throw, _)
 
   local client_versions = vim.lsp.util.buf_versions or {}
 
+  -- backwards compatibility with easy-dotnet < 3.4.18
+  local contains_create_file_edit = vim.iter(params.documentChanges):any(function(change) return change.kind == "create" end)
+
   for _, change in ipairs(params.documentChanges) do
     if change.textDocument and change.textDocument.uri then
-      local bufnr = vim.fn.bufnr(vim.uri_to_fname(change.textDocument.uri))
+      local bufnr = not contains_create_file_edit and vim.uri_to_bufnr(change.textDocument.uri) or vim.fn.bufnr(vim.uri_to_fname(change.textDocument.uri))
       if bufnr ~= -1 then
         local current_ver
         if type(client_versions) == "function" then
@@ -32,9 +35,7 @@ return function(params, response, throw, _)
     local uri = change.textDocument and change.textDocument.uri
     if type(uri) == "string" then
       local bufnr = vim.fn.bufnr(vim.uri_to_fname(uri))
-      if vim.api.nvim_buf_is_valid(bufnr) then vim.schedule(function()
-        pcall(vim.api.nvim_buf_call, bufnr, function() vim.cmd("silent! update") end)
-      end) end
+      if vim.api.nvim_buf_is_valid(bufnr) then pcall(vim.api.nvim_buf_call, bufnr, function() vim.cmd("silent! update") end) end
     end
   end
 
